0bc7a7308f69e00b3b133a8f55b6518ab7e752eb
Tomasivb <omigroom@gmail.com>
2021-05-16 00:31:30 -0400

    Avances

--- code.js
+++ code.js
@@ -7,7 +7,42 @@ $(document).ready(function(){
     var c3=$("#C3");
     var c4=$("#C4");
     var graficos=$("#Graf_Mat");
+    var rama=$(".img")
 
+/*
+    var width= 300
+    height=300
+    margin=20
+    var radius = Math.min(width,height)/2 - margin;
+    var svg=d3.select("#bloque")
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height)
+    .append("g")
+    .attr("transform","translate("+width/2+","+height/2+")");
+    var datos= {h:51,m:49};
+    var color = d3.scaleOrdinal()
+    .domain(datos)
+    .range(["#b3e6e0","#49ada4"]);
+
+    var pie= d3.pie()
+        .value(function(d){
+            return d.value;
+        });
+    var datosl=pie(d3.entries(datos));
+
+    svg.selectAll("#bloque")
+    .data(datosl)
+    .enter()
+    .append("path")
+    .attr('d',d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius))
+    .attr('fill',function(d){return(color(d.datos.key))})
+    .attr("stroke","black")
+    .style("stroke-width","2px")
+    .style("opacity",0.7);
+*/
     menu.click(function(){
         carreras.toggle();
     })
@@ -24,10 +59,15 @@ $(document).ready(function(){
         graficos.text("Grafico carrera 4");
     })
     area.mouseenter(function(){
-        $(this).css("background","#252525")
+        $(this).css("background","#252525");
+        //var nombrerama=$(this).attr("id");
+        //$("#Nombre_Rama").text(nombrerama);
     })
     area.mouseleave(function(){
-        $(this).css("background","#403f49")
+        $(this).css("background","#403f49");
+        //$("#Nombre_Rama").text("")
     })
+
+
 
 })
